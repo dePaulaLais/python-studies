a02a35382750ac5b8f069d58d5ea1104635902e2
Lapaula <lapaula@brasilseg.com.br>
2023-02-21 12:49:59 -0300

    Testando if, elif e else

---
+++ car_speedtest.py
@@ -0,0 +1,9 @@
+velocidade = 50
+
+if velocidade > 110:
+    print('Acima da velocidade permitida.')
+    print('Favor reduzir a velocidade.')
+elif velocidade < 110:
+    print('Aumente a velocidade, sr')
+else:
+    print('Velocidade OK!')
