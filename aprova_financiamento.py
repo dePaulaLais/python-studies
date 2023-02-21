748bda7d5c80820af1cc33dfeefbbce64d5f9a99
Lapaula <lapaula@brasilseg.com.br>
2023-02-21 12:57:11 -0300

    Logical Operators test

---
+++ aprova_financiamento.py
@@ -0,0 +1,9 @@
+#Operadores lógicos (Logical Operators)
+
+renda_acima_5mil = True
+nome_limpo = False
+
+if renda_acima_5mil and nome_limpo:
+    print('Aprroved!Congrats')
+else:
+    print('I am sorry, try again later.')
