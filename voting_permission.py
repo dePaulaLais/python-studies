35a0060098a5d892ea2f7c881ce61607feb13ccb
Lapaula <lapaula@brasilseg.com.br>
2023-02-21 13:04:58 -0300

    Ternary Operator test

---
+++ voting_permission.py
@@ -0,0 +1,11 @@
+# Operador ternário (Ternary Operator)
+
+idade = 15
+
+if idade >= 16:
+    resultado = print('Voto permitido.')
+else:
+    resultado = print ('Voto não permitido.')
+
+#### OU #####
+resultado = 'Voto permitido.' if idade >= 16 else 'Voto não permitido.'
